1e69ea3cd8ed7449ab08c8f65a1443cc69fdd2eb
Niklas <niklas.a.rist@gmail.com>
2025-06-09 08:45:59 +0200

    +

---
+++ Spiel/wallet/src/test/java/at/fhv/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
